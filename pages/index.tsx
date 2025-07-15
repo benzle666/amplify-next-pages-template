d154688c3dee70826b43e6f1222c0202987d7062
benzle666 <155555943+benzle666@users.noreply.github.com>
2025-07-15 11:59:29 +0700

    added per-user data isolation again

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import { useAuthenticator } from "@aws-amplify/ui-react";
 
+// Push per user again
+
 const client = generateClient<Schema>();
 
 export default function App() {
