74ef524a1a8dc3ca0210bfef830c4a43ca75a6f9
benzle666 <155555943+benzle666@users.noreply.github.com>
2025-07-15 12:09:24 +0700

    fix astrophies

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,6 @@ import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import { useAuthenticator } from "@aws-amplify/ui-react";
 
-// Push per user again
-
 const client = generateClient<Schema>();
 
 export default function App() {
@@ -34,7 +32,7 @@ export default function App() {
 
   return (
     <main>
-      <h1>{user?.signInDetails?.loginId}'s todos</h1>
+      <h1>{user?.signInDetails?.loginId} todos</h1>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
